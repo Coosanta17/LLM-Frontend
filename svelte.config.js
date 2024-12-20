7e76397e7bdd827d258a1f8d41ba43e2ff38f23c
Coosanta <coosantamapy@gmail.com>
2024-12-20 12:26:32 +1100

    Add layout configuration and update SvelteKit settings

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter({
-			// Directory for the built files
-			pages: 'build',
-			assets: 'build',
-			fallback: null // Use 'index.html' if you have client-side routing
+			fallback: '404.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'development' ? '' : '/LLM-Frontend'
-		},
-		appDir: 'internal'
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
