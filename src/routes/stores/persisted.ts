8d72570ae72d591a3b55751013bfe48a426f8dad
Coosanta <coosantamapy@gmail.com>
2025-01-30 20:36:10 +1100

    Refactor chat update logic to handle null updates with more readability.

--- src/routes/stores/persisted.ts
+++ src/routes/stores/persisted.ts
@@ -1,5 +1,5 @@
 import { persisted } from "svelte-persisted-store";
-import { writable } from "svelte/store";
+import { writable, get } from "svelte/store";
 
 import type { Chat } from "../types";
 
@@ -16,21 +16,21 @@ export const chats = persisted<Chat[]>('chats', [])
 export const selectedChat = writable<Chat>(initialChat);
 
 selectedChat.subscribe((updatedChat) => {
-    if (updatedChat) {
-        chats.update((currentChats) => {
-            const chatIndex = currentChats.findIndex(chat => chat.uuid === updatedChat.uuid);
+    if (!updatedChat) return;
 
-            if (chatIndex !== -1) {
-                if (currentChats[chatIndex] !== updatedChat) {
-                    return [
-                        ...currentChats.slice(0, chatIndex),
-                        { ...updatedChat },
-                        ...currentChats.slice(chatIndex + 1),
-                    ];
-                }
+    chats.update((currentChats) => {
+        const chatIndex = currentChats.findIndex(chat => chat.uuid === updatedChat.uuid);
+
+        if (chatIndex !== -1) {
+            if (currentChats[chatIndex] !== updatedChat) {
+                return [
+                    ...currentChats.slice(0, chatIndex),
+                    { ...updatedChat },
+                    ...currentChats.slice(chatIndex + 1),
+                ];
             }
+        }
 
-            return currentChats;
-        });
-    }
+        return currentChats;
+    });
 });
